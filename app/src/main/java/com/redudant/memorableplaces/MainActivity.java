f7ae23266777b649e66d6aa53c14ebff2739b2bb
maulana2ahmad <maulana2ahmad@gmail.com>
2019-05-18 23:40:30 +0700

    update list location

--- app/src/main/java/com/redudant/memorableplaces/MainActivity.java
+++ app/src/main/java/com/redudant/memorableplaces/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 
@@ -14,31 +15,39 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
+    static ArrayList<String> places = new ArrayList<> ();
+    static ArrayList<LatLng> locations = new ArrayList<> (); //membuat dafar list lokasi
+
+    static ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
 
-        listView = (ListView) findViewById(R.id.listView);
+        listView = findViewById ( R.id.listView );
 
-        final ArrayList<String> places = new ArrayList<>();
+        places.add ( "Add a new places..." );
 
-        places.add("Add a new places...");
+        locations.add ( new LatLng ( 0, 0 ) ); //menambahkan
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+        arrayAdapter = new ArrayAdapter<> ( this, android.R.layout.simple_list_item_1, places );
 
-        listView.setAdapter(arrayAdapter);
+        listView.setAdapter ( arrayAdapter );
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener ( new AdapterView.OnItemClickListener () {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 
                 Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-                intent.putExtra("placesNumber", position);
+                intent.putExtra("placeNumber", position);
 
                 startActivity(intent);
             }
+
         });
+
+
     }
 }
--- app/src/main/java/com/redudant/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/redudant/memorableplaces/MapsActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,6 +25,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
@@ -34,52 +42,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //method ini menambahkan hasil izin
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult ( requestCode, permissions, grantResults );
 
         //kemudian kita cek apakah hsail grand lebih bersar dari nol
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission ( this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                locationManager.requestLocationUpdates ( LocationManager.GPS_PROVIDER, 0, 0, locationListener );
 
                 //kaemudian kami mendapatkan lokasi user dan kami kirimkan lokasi user pada peta menggunkan lokasi terakhir user yang diketahui
-                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Location lastKnownLocation = locationManager.getLastKnownLocation ( LocationManager.GPS_PROVIDER );
 
                 //dan lokasi user akan menjadi judul
-                centerMapsOnLocation(lastKnownLocation, "Your location.");
+                centerMapOnLocation ( lastKnownLocation, "Your location." );
             }
         }
     }
 
     //untuk membuat titik lokasi baru
-    public void centerMapsOnLocation(Location location, String title) {
+    public void centerMapOnLocation(Location location, String title) {
 
         //menetukan locasi user
-        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng userLocation = new LatLng ( location.getLatitude (), location.getLongitude () );
 
-        mMap.clear(); //menghapus semua penanda location
+        mMap.clear (); //menghapus semua penanda location
 
         //lakukan pemerikasaan apakah judul sama dengan lokasi anda
         if (title != "Your location") {
 
             //menambah penanda baru dan lokasi yang kita gunakan (userLocation) judul lokasi kita berada
-            //maka kita akan menambahkan arameter String title pada method centerMapsOnLocation
-            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+            //maka kita akan menambahkan arameter String title pada method centerMapOnLocation
+            mMap.addMarker ( new MarkerOptions ().position ( userLocation ).title ( title ) );
         }
 
         //ini akam zoom penanda lokasi kita dan jarak nya bebas disini saya menggunakan 10
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10));
+        mMap.moveCamera ( CameraUpdateFactory.newLatLngZoom ( userLocation, 10 ) );
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_maps );
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager ()
+                                                                      .findFragmentById ( R.id.map );
+        mapFragment.getMapAsync ( this );
     }
 
 
@@ -97,25 +105,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         //menambahkan kode ini untu mendapatkan peta google
-        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMapLongClickListener ( this );
 
-        Intent intent = getIntent();
+        Intent intent = getIntent ();
         //Toast.makeText(getApplicationContext(), intent.getStringExtra("placesNumber"), Toast.LENGTH_LONG).show();
 
-        if (intent.getIntExtra("placesNumber", 0) == 0) {
+        if (intent.getIntExtra ( "placesNumber", 0 ) == 0) {
 
             //Zoom in on user's location
 
             //kita akan mendapatkan lokasi pengguna dengan locationmanager
-            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationManager = (LocationManager) this.getSystemService ( Context.LOCATION_SERVICE );
 
             //ini akan memberikan metode informasi lokasi baru
-            locationListener = new LocationListener() {
+            locationListener = new LocationListener () {
                 @Override
                 public void onLocationChanged(Location location) {
 
-                    //kami akan memusatkan lokasinya dan kita letakan method centerMapsOnLocation disini
-                    centerMapsOnLocation(location, "your location..");
+                    //kami akan memusatkan lokasinya dan kita letakan method centerMapOnLocation disini
+                    centerMapOnLocation ( location, "your location.." );
 
                 }
 
@@ -137,45 +145,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             try {
 
+                //meminta pembaharuan lokasi
                 //sekarang kita cek kemali, apakah pengguna menggunakan versi 23 marshmellow
                 if (Build.VERSION.SDK_INT > 23) {
 
                     //kami meminta pembaruan lokasi dan Lparameter Location kita ambil dari method
                     //GPS_PROVIDER 0,0 saya pikir jarak dan waktu dari pembaruagetLastKnownLocationn terakhir dari LocationListener mengigatkan saya pada manifes pada saat
                     //kita membuat MapsActivity <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                    locationManager.requestLocationUpdates ( LocationManager.GPS_PROVIDER, 0, 0, locationListener );
 
                 } else {
                     //kalo tidak, kita perlu memerikas apakah kita punya izin akses lokasi dengan PackageManager.PERMISSION_GRANTED mendapatkan izi yang diberikan user
-                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission ( this, Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
                         //jadi itu artinya kita mendapatkan izin dari user dan mendapatkan lokasi user dan kami mendapatkan lokasi yang diketahui
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                        locationManager.requestLocationUpdates ( LocationManager.GPS_PROVIDER, 0, 0, locationListener );
 
                         //kaemudian kami mendapatkan lokasi user dan kami kirimkan lokasi user pada peta menggunkan lokasi terakhir user yang diketahui
-                        Location lastKnowLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        Location lastKnowLocation = locationManager.getLastKnownLocation ( LocationManager.GPS_PROVIDER );
 
                         //dan lokasi user akan menjadi judul
-                        centerMapsOnLocation(lastKnowLocation, "Your location.");
+                        centerMapOnLocation ( lastKnowLocation, "Your location." );
 
                     } else {
 
-                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                        ActivityCompat.requestPermissions ( this, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION }, 1 );
                     }
-                }
 
+
+                }
+            } catch (SecurityException e) {
+
+                Log.e ( "Exception: %s", e.getMessage () );
             }
-            catch (SecurityException e)  {
-                Log.e("Exception: %s", e.getMessage());
-            }
+
+
+        } else {
+
+            Location placeLocation = new Location(LocationManager.GPS_PROVIDER);
+            placeLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber", 0)).latitude);
+            placeLocation.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber", 0)).longitude);
+
+            centerMapOnLocation(placeLocation, MainActivity.places.get(intent.getIntExtra("placeNumber", 0)));
 
         }
+
     }
+
 
     @Override
     public void onMapLongClick(LatLng latLng) {
 
+        //memberikan nama dan tepat dengan class Geocoding
+        Geocoder geocoder = new Geocoder ( getApplicationContext (), Locale.getDefault () );
+
+        //kemudian kita akan mengambil alamat yang di definisikan sebagai String dan awalnya akan kita buat kosong
+        String address = "";
+
+
+        try {
+
+            //menambah List daftar alamat dari geocode dan hasilnya 1
+            List<Address> listAddress = geocoder.getFromLocation ( latLng.latitude, latLng.longitude, 1 );
+
+            if (listAddress != null & listAddress.size () > 0) {
+
+                //kita akan cek dan mengambil jalan raya
+                if (listAddress.get ( 0 ).getThoroughfare () != null) {
+
+                    // kita akan melakukan pemeriksaan lagi untuk mendapatkan beberapa jalan
+                    if (listAddress.get ( 0 ).getSubThoroughfare () != null) {
+
+                        //jika sub jalan itu ada makan kita tampilkan
+                        address += listAddress.get ( 0 ).getSubThoroughfare () + " ";
+
+                    }
+
+                    //jika jalan itu ada kita tamilkan
+                    address += listAddress.get ( 0 ).getThoroughfare ();
+
+                }
+
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace ();
+        }
+
+        //apakah alamat masih kosong
+        if (address == "") {
+
+            //jika alamat tiddak terdeteksi maka kita akan menampilkan tangan jam dan tanggal
+            SimpleDateFormat sdf = new SimpleDateFormat ( "HH:mm a dd-MM-yyyy" );
+            address = sdf.format ( new Date () );
+        }
+
+
         //penanda peta tempat yang baru
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Your new memorable places"));
+        mMap.addMarker ( new MarkerOptions ().position ( latLng ).title ( address ) );
+
+
+        MainActivity.places.add ( address );
+        MainActivity.locations.add ( latLng );
+
+        //memeritahu data set berubah
+        MainActivity.arrayAdapter.notifyDataSetChanged ();
+
+        Toast.makeText ( getApplicationContext (), "Location save.", Toast.LENGTH_LONG ).show ();
     }
 
 }
